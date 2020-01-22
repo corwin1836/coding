a06441e34d2c8576c2be74f1d7b15674d5df0873
ian <imartin1812@gmail.com>
2020-01-21 21:46:01 -0800

    Began 7.1 discovered bug when running in user time with unspecified number of ticks for program args.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,43 @@
 package com.company;
 
+import static java.lang.Thread.sleep;
+
 public class Main {
     private static int tickCounter = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, Exception {
+        if (args.length == 0) {
+            throw new Exception("Required program parameters not met, exiting program.");
+    }
         Timer timer = new Timer();
-
         Airport laguardia = new Airport(3, 100);
         while (goSequence(args)) {
             timer.printCurrentTime();
             laguardia.airportControl();
             tickCounter++;
             timer.tick();
+            sleep(programRunTime(args));
         }
     }
 
     public static boolean goSequence(String[] args) {
         int userInput;
-        if (args.length == 0) {
+        if (args.length < 2) {
             return true;
         } else {
-            userInput = Integer.parseInt(args[0]);
+            userInput = Integer.parseInt(args[1]);
         }
         return tickCounter < userInput;
+    }
+
+    public static int programRunTime(String [] args) {
+
+        String runSpeed = args[0];
+        if (runSpeed.equals("u")) {
+            return 1000;
+        } else {
+            return 0;
+        }
+
     }
 }
