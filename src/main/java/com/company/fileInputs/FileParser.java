f13edba067b41757bdacfdc369356b35ad0da1ba
ian <imartin1812@gmail.com>
2020-02-09 21:31:32 -0800

    Adding functionality to the fileParser to convert everything from Gson to internal classes.

--- src/main/java/com/company/fileInputs/FileParser.java
+++ src/main/java/com/company/fileInputs/FileParser.java
@@ -1,9 +1,6 @@
 package com.company.fileInputs;
 
-import com.company.Airplane;
-import com.company.AirplaneMake;
-import com.company.AirplaneModel;
-import com.company.RandomNumberGenerator;
+import com.company.*;
 import com.company.interfaces.NumberGenerator;
 import com.company.interfaces.RefuelDelegate;
 import com.company.models.Airline;
@@ -12,29 +9,33 @@ import com.company.models.TrafficControl;
 import com.google.gson.Gson;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class FileParser{
     private TrafficControl trafficControl;
     private NumberGenerator gen = new RandomNumberGenerator();
+
     public FileParser(MyFileReader quickReader) throws IOException {
         Gson gson = new Gson();
         trafficControl = gson.fromJson(quickReader.reader("/home/ian/Documents/coding/inputs/planes.json"), TrafficControl.class);
     }
 
-    public Airplane[] buildOutgoing (NumberGenerator generator, RefuelDelegate delegate) {
+    public Map<String, Airplane[]> buildOutgoing (NumberGenerator generator, RefuelDelegate delegate) {
         Map<String, Airports> airportsMap = trafficControl.getAirports();
         Set<String> keyset = airportsMap.keySet();
+        Airports airports;
+        Airline[] airlines = new Airline[keyset.size()];
         for(String iterator : keyset) {
-            Airports airports = airportsMap.get(iterator);
-            Airline[] airlines = airports.getPlanes();
+            airports = airportsMap.get(iterator);
+            airlines = airports.getPlanes();
         }
-        Airplane[] airplanes = new Airplane[planes.length];
-        for (int i = 0; i < airports.length; i += 1) {
-            String model = airports[i].getModel();
-            String route = airports[i].getRoute();
-
+        Airplane[] airplanes = new Airplane[airlines.length];
+        for (int i = 0; i < airlines.length; i += 1) {
+            String make = airlines[i].getMake();
+            String model = airlines[i].getModel();
+            String route = airlines[i].getRoute();
             airplanes[i] = new Airplane(
                     generator,
                     delegate,
@@ -43,6 +44,8 @@ public class FileParser{
                     route);
             airplanes[i].setMaxFuel();
         }
-        return airplanes;
+        Map<Airport, Airplane[]> airportAndFleet = new HashMap<>();
+        int runways = airports.getRunways();
+        return ;
     }
 }
