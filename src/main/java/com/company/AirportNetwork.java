210fcc6e86d191d85a0ea02899ac76437877d0ba
ian <imartin1812@gmail.com>
2020-02-16 21:28:11 -0800

    Started to reconfigure AirportNetwork to hold all the airports.

--- src/main/java/com/company/AirportNetwork.java
+++ src/main/java/com/company/AirportNetwork.java
@@ -9,14 +9,7 @@ import java.util.Set;
 
 public class AirportNetwork {
 
-    private Map<Airport, AirlineFleet> network = new HashMap<>();
-
-    public AirportNetwork(Map<String, Airports> airportsMap, AirlineFleet fleet) {
-        Set<String> keySet = airportsMap.keySet();
-        for (String key : keySet) {
-            Airports airports = airportsMap.get(key);
-            int runways = airports.getRunways();
-            network.put(new Airport(runways), fleet);
-        }
+    public AirportNetwork() {
+        
     }
 }
